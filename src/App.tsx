07d45905d308b88ec758514c8db0d34aff1db0c7
avielmanagement <avi@dealflowgroupny.com>
2026-03-08 10:05:19 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const App: React.FC = () => {
 
         {/* Legal pages */}
         <Route path="/privacy" element={<PrivacyPolicy />} />
-        <Route path="/TermsOfService" element={<Terms />} />
+        <Route path="/TermsOfService" element={<TermsOfervice />} />
 
         {/* Fallback */}
         <Route path="*" element={<Navigate to={PageRoute.HOME} replace />} />
